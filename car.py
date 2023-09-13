b6b458e2f0def15ba67d52b5124c533a65d37eed
Abigail Bareiss <abareiss@uci.edu>
2023-09-13 11:24:47 -0700

    renamed

--- car.py
+++ car.py
@@ -1,10 +1,12 @@
-from abc import ABC, abstractmethod
+from serviceable import Serviceable
+from engine.engine import Engine
+from battery.battery import Battery
 
+class Car():
+    def __init__(self, engine_type, battery_type):
+        self.engine = Engine(engine_type)
+        self.battery = Battery(battery_type)
 
-class Car(ABC):
-    def __init__(self, last_service_date):
-        self.last_service_date = last_service_date
-
-    @abstractmethod
     def needs_service(self):
-        pass
+        self.servicable = Serviceable(self.engine, self.battery)
+        return self.serviceable.needs_service()
